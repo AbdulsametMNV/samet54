410068a85f90896e4ba10d2fd84df13a6f5ddddb
AbdulsametMNV <mnvsamet54@gmail.com>
2025-01-01 19:32:52 +0300

    Delete app directory

--- app/Views/tema/footer.php
+++
@@ -1,6 +0,0 @@
-      <footer>
-        “Bana adını bahşet, yabancı, sonrada niye seni öldürmemem gerektiğini anlat." 
-      </footer>
-    </div>
-  </body>
-</html>
--- app/Views/tema/panel_footer.php
+++
@@ -1,7 +0,0 @@
-</div>
-<footer>
-        <p>&copy; 2024 Admin Paneli. Tüm Hakları Saklıdır.</p>
-    </footer>
-
-</body>
-</html>
